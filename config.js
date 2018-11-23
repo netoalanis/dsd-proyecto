fe890d9e139be076a2b1456940ba7b1990efa734
Ernesto Alanis <44209472+netoalanis@users.noreply.github.com>
2018-11-22 19:53:04 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 module.exports = {
-    consumer_key: '9AtP7iWy8XYCOP4tRHoByfBwH',
-    consumer_secret: 'yHjpYVlZh6UMW2H1i8prsWVXCsTyoJqUciq80tpI7oyyDDrw9H',
-    access_token_key: '40956992-BlReGcPFP7kDp5SN1iQ4i3tzqcclk5eJFxrceyPys',
-    access_token_secret: 'FjuoJfX1Sr2XAr7fnHh66Quk9yTlrokLA8UDmg23ReOgG'
+    consumer_key: 'A9AtP7iWy8XYCOP4tRHoByfBwH',
+    consumer_secret: 'AyHjpYVlZh6UMW2H1i8prsWVXCsTyoJqUciq80tpI7oyyDDrw9H',
+    access_token_key: 'A40956992-BlReGcPFP7kDp5SN1iQ4i3tzqcclk5eJFxrceyPys',
+    access_token_secret: 'AFjuoJfX1Sr2XAr7fnHh66Quk9yTlrokLA8UDmg23ReOgG'
   }
